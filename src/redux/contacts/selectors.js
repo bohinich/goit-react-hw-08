3f790856e20bebbb5a56d7feeeaf880eaef643f3
Bohinich Serhii <bohinich1@gmail.com>
2025-06-15 16:46:43 +0300

    .

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -1,3 +1,5 @@
 export const selectContacts = state => state.contacts.items;
 export const selectIsLoading = state => state.contacts.isLoading;
 export const selectError = state => state.contacts.error;
+export const selectLoading = state => state.contacts.loading;
+
